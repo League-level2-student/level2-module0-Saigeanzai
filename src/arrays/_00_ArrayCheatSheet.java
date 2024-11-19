f386683f22d4490e7fc6c9387f48374f5a864de2
league <league@imac-38.attlocal.net>
2024-11-18 16:57:18 -0800

    I did array cheat sheet

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,19 +3,25 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] variable = {"first","second","third","forth","fifth"};
 		//2. print the third element in the array
-		
+		System.out.println(variable[3]);
 		//3. set the third element to a different value
-		
+		variable[3] = "notForth";
 		//4. print the third element again
-		
+		System.out.println(variable[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i = 0; i<5; i++) {
+			System.out.println(variable[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] num = new int[50];
+		for(int i =0; i<50; i++) {
+			num[i]=i;
+			//System.out.println(i);
+		}
 		//7. use a for loop to make every value of the integer array a random number
 		
 		//8. without printing the entire array, print only the smallest number in the array
